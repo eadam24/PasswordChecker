34f84768975a9cb7b7cc3b7756d994957dc57850
e.adam24 <e.adam24@LAB108PC04>
2023-10-10 15:30:02 +0300

    Add project files.

---
+++ Classes/PassChecker.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordChecker.Classes
+{
+    class PassChecker
+    {
+        private string password;
+        private string[] specialChar = { "~", "!", "@", "#", ".", "$", "%", "^", "&", "*", "-", "," };
+        char[] UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        char[] LowerCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToLower().ToCharArray();
+        char[] Nums = "1234567890".ToCharArray();
+
+
+        private bool containsSpecialChars = false;
+        private bool containsUpper = false;
+        private bool containsLower = false;
+        private bool containsNum = false;
+        private bool isShort = true;
+
+        public bool isStrong { get; set; }
+        public PassChecker(string password)
+        {
+            this.password = password;
+            isStrong = false;
+        }
+
+        public string CheckPassword()
+        {
+            if(password == null) { return "No password provided"; }
+
+            string result = string.Empty;
+
+            if (password.Length <= 8)
+            {
+                result = result + "Password is too short!";
+                isShort = true;
+            }
+            else
+            {
+                isShort = false;
+            }
+
+            foreach(string c in specialChar)
+            {
+                if (password.Contains(c))
+                {
+                    containsSpecialChars = true;
+                }
+            }
+
+            if(!containsSpecialChars)
+            {
+                result = result + "\nAdd special characters!";
+            }
+
+            foreach(char u in UpperCase)
+            {
+                if (password.Contains(u))
+                {   
+                    containsUpper = true;
+                }
+            }
+
+            if (!containsUpper)
+            {
+                result = result + "\nAdd Upper Case!";
+            }
+
+            foreach (char u in LowerCase)
+            {
+                if (password.Contains(u))
+                {
+                    containsLower = true;
+                }
+            }
+
+            if (!containsLower)
+            {
+                result = result + "\nAdd Lower Case!";
+            }
+            foreach (char u in Nums)
+            {
+                if (password.Contains(u))
+                {
+                    containsNum = true;
+                }
+            }
+
+            if (!containsNum)
+            {
+                result = result + "\nAdd Numbers!";
+            }
+
+            if (containsLower && containsNum && containsSpecialChars && containsUpper && !isShort)
+            {
+                isStrong = true;
+                return "Password is strong!";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using PasswordChecker.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PasswordChecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PassChecker check = new PassChecker(password.Text);
+                
+                Result.Text = check.CheckPassword().Trim().Replace(Environment.NewLine, "");
+            if (check.isStrong)
+            {
+                Result.Foreground = Brushes.Green;
+            }
+            else
+                Result.Foreground = Brushes.Red;
+            
+
+
+        }
+    }
+}
